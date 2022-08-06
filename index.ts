e72a6c22556eb317bf721048cf6a9cbd0342ad69
Timothy Ongko <tong0006@student.monash.edu>
2022-08-06 19:08:34 +1000

    Extract raw data ID from request

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
-import { config as dotenv_config} from "dotenv";
-import UPLOAD_ROUTE from './src/Upload/UploadController';
+import { config as dotenv_config } from "dotenv";
+import UPLOAD_ROUTE from "./src/Upload/UploadController";
 
 dotenv_config();
 
@@ -9,9 +9,21 @@ const port = process.env.PORT;
 
 app.use("/upload", UPLOAD_ROUTE);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server is running");
+app.get("/raw-data/:rawDataID", (req: Request, res: Response) => {
+	const rawDataID = req.params["rawDataID"];
+	res.send(
+		"A request to obtain raw data of ID " +
+			rawDataID +
+			" has been acknowledged"
+	);
 });
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Express + TypeScript Server is running");
+});
+
 app.listen(port, () => {
-  console.log(`⚡️[server]: Brand new Server is running at https://localhost:${port}`);
+	console.log(
+		`⚡️[server]: Brand new Server is running at https://localhost:${port}`
+	);
 });
