d8b2071dff461581cd06032e7aa069dec4b237db
Teigan <tros0007@student.monash.edu>
2022-08-15 23:09:17 +1000

    Removed some unnecessary code accidentally left in from recent merge

--- src/router.ts
+++ src/router.ts
@@ -1,22 +1,12 @@
-import { Router, Request, Response } from "express";
-import databaseConnection, {
-	toDBDate,
-	PlotCollection,
-	FilePointer,
-	Upload,
-} from "./databaseConnection";
-import { v4 as uuidv4 } from "uuid";
-import uploadController from "./Upload/UploadController";
-import shareController from "./Share/ShareController"
-import rawDataController from "./RawData/RawDataController"
-import { readFile, saveFile } from "./RawData/storageController";
+import { Router } from "express";
 
-// Until accounts are added, all data with be under this user
-const TEMP_USER = "temp";
+import uploadController from "./Upload/UploadController";
+import shareController from "./Share/ShareController";
+import rawDataController from "./RawData/RawDataController";
 
 const router = Router();
 router.use("/upload", uploadController);
-router.use("/share", shareController)
-router.use("/raw-data", rawDataController)
+router.use("/share", shareController);
+router.use("/raw-data", rawDataController);
 
 export default router;
