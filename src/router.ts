47b8d94547cfb05e9717cefa7dd64e6debde5664
Akbar Fadiansyah <akbarfadi01@gmail.com>
2022-08-23 19:23:05 +1000

    Change router use for /user for consistency

--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,12 @@ import { Router } from "express";
 import uploadController from "./Upload/UploadController";
 import shareController from "./Share/ShareController";
 import rawDataController from "./RawData/RawDataController";
-import USER_ROUTE from "./User/UserController";
 import {invalidCredentialsErrorHandler, invalidSignUpErrorHandler} from "./ExpressErrorHandlers";
+import userController from "./User/UserController";
 
 const router = Router();
 router.use("/upload", uploadController);
-router.use("/user", USER_ROUTE);
+router.use("/user", userController);
 router.use("/share", shareController);
 router.use("/raw-data", rawDataController);
 
