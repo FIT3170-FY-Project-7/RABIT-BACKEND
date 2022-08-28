a21f9ce57b68cb8f92eb567413140e13e10a0997
RHackers-00 <rhac0001@student.monash.edu>
2022-08-28 10:13:29 +1000

    get all plot collections for a user id

--- src/RawData/uploadSql.ts
+++ src/RawData/uploadSql.ts
@@ -14,3 +14,6 @@ SELECT * FROM plot_collection
 export const GET_PLOT_COLLECTION = `
 SELECT * FROM (plot_collection p JOIN file_pointer f on p.collection_id = f.collection_id) JOIN upload u ON f.upload_id = u.upload_id WHERE p.collection_id = ?
 `;
+
+export const GET_COLLECTIONS_FOR_USER = `
+SELECT p.Collection_title, c.Last_modified FROM Plot_Collection p JOIN Corner_Plot c on p.Collection_id = c.Collection_id AND c.user_id = (SELECT TOP 1 cp.user_id FROM Corner_Plot cp WHERE cp.Collection_id = p.Collection_id AND cp.user_id = (?)) ORDERBY c.last_modified`;
