ea45f7691681b86503dd44315bd51a59b56b379f
RHackers-00 <rhac0001@student.monash.edu>
2022-08-30 22:15:34 +1000

    Made get user collections ordered

--- src/RawData/uploadSql.ts
+++ src/RawData/uploadSql.ts
@@ -16,4 +16,4 @@ SELECT * FROM (plot_collection p JOIN file_pointer f on p.collection_id = f.coll
 `;
 
 export const GET_COLLECTIONS_FOR_USER = `
-SELECT p.collection_title, p.collection_id, MAX(c.last_modified) as last_modified from corner_plot c JOIN plot_collection p on p.collection_id = c.collection_id GROUP BY p.collection_id`;
+SELECT p.collection_title, p.collection_id, MAX(c.last_modified) as last_modified FROM (corner_plot c JOIN plot_collection p on p.collection_id = c.collection_id WHERE c.user_id = (?)) GROUP BY p.collection_id ORDER BY last_modified`;
